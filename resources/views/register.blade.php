f2f928f1d232f29c1022c8113b4bfe6ce7d8bca9
huangxinyi <huangxinyi@lianjia.com>
2017-04-19 16:20:54 +0800

    add register feature and modify UI size

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <div class="special-form">
-        <a href="{{ route('home') }}"><img src="{{ \App\Helpers\Helpers::logoUrl()  }}" alt=""></a>
-        <h3 class="text-center">REGISTER</h3>
+        <a href="{{ route('home') }}">{{--<img src="{{ \App\Helpers\Helpers::logoUrl()  }}" alt="">--}}</a>
+        <h1 class="text-center">REGISTER</h1>
         @if ($errors->first())
             <span class="status-msg error-msg">{{ $errors->first() }}</span>
         @endif
